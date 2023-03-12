1c7cdcf271e0a52b56ae46273ee107fd72f88088
Kevin Jonathan <kevinjonathan3010@gmail.com>
2023-03-13 00:41:13 +0700

    experiment with vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import '@/styles/globals.css';
 import { useState } from 'react';
 import AppContext from '../context/AppContext';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   const [darkMode, setDarkMode] = useState(false);
 
-  return (<AppContext.Provider value={{ darkMode, setDarkMode }}><Component {...pageProps} /></AppContext.Provider>)
+  return (
+    <AppContext.Provider value={{ darkMode, setDarkMode }}>
+      <Component {...pageProps} />
+      <Analytics />
+    </AppContext.Provider>
+  )
 }
