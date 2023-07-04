595ae16409cb301180e09cdba8867abcc8a8efa2
Kevin Jonathan <kevinjonathan3010@gmail.com>
2023-07-04 22:58:25 +0700

    Add scroll indicator using framer motion

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,16 @@ import Portfolio from "../components/portfolio.js";
 import AppContext from "../context/AppContext.js";
 import { useContext } from "react";
 import Layout from "./layout.js";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 export default function Home() {
   const context = useContext(AppContext);
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
 
   return (
     <Layout>
@@ -20,6 +27,7 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <main className="bg-stone-50 px-10 dark:bg-gray-900 md:px-20 lg:px-40">
+          <motion.div className="progress-bar bg-blue-600 dark:bg-blue-700" style={{ scaleX }} />
           <Header />
           <Services />
           <Articles />
