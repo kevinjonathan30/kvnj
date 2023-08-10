68a5a9a0b128fbc873355c704631ea3c4ba0edf1
Kevin Jonathan <kevinjonathan3010@gmail.com>
2023-08-10 18:15:54 +0700

    Improve motion reduce function

--- components/include/MotionLinker.js
+++ components/include/MotionLinker.js
@@ -1,6 +1,6 @@
 export default function MotionLinker({ children, href, openInNewTab=true, className }) {
     return (
-        <div className="transition-all motion-reduce:transition-none hover:scale-110 active:scale-90 ease-in-out">
+        <div className="transition-all motion-reduce:transition-none hover:scale-110 motion-reduce:hover:scale-100 active:scale-90 motion-reduce:active:scale-100 ease-in-out">
             <a href={href} target={openInNewTab ? "_blank" : ""} rel="noopener" className={className}>
                 {children}
             </a>
