befc1710a1b3fe5ca88201010ff9470ad03edec1
Kevin Jonathan <kevinjonathan3010@gmail.com>
2023-08-14 18:12:44 +0700

    Adjust article text size

--- pages/500.js
+++ pages/500.js
@@ -4,7 +4,7 @@ import AnimatedAnchor from "../components/include/AnimatedAnchor";
 import AppContext from "../context/AppContext.js";
 import Button from '../components/include/Button';
 
-export default function NotFound404() {
+export default function InternalServerError500() {
     const context = useContext(AppContext);
 
     return (
@@ -28,13 +28,13 @@ export default function NotFound404() {
                     </nav>
                     <div className="text-center p-10">
                         <h2 className="font-gloriaHallelujah text-5xl py-2 text-blue-600 font-semibold tracking-wide dark:text-blue-400 md:text-6xl">Oops!</h2>
-                        <h3 className="font-gloriaHallelujah text-2xl py-2 dark:text-white md:text-3xl mb-4">404 - Not Found</h3>
+                        <h3 className="font-gloriaHallelujah text-2xl py-2 dark:text-white md:text-3xl mb-4">500 - Internal Server Error</h3>
                         <p className="text-md md:text-xl font-normal py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-lg mx-auto">
-                            Perhaps the page you are looking for has risen into the unknown...?
+                            Perhaps the server needs time to recover itself...?
                         </p>
                     </div>
                     <div className="flex justify-center mt-8">
-                        <Button href={"./"} openInNewTab={false}>Back to Site Home</Button>
+                        <Button href={"./"} openInNewTab={false}>Try Again</Button>
                     </div>
                 </section>
             </main>
