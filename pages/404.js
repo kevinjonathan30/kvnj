fe26de66dd6a3f0b04b88754fa1af9bea005e52a
Kevin Jonathan <kevinjonathan3010@gmail.com>
2023-08-14 17:57:16 +0700

    Update 404 page font

--- pages/404.js
+++ pages/404.js
@@ -27,8 +27,8 @@ export default function NotFound404() {
                         </ul>
                     </nav>
                     <div className="text-center p-10">
-                        <h2 className="text-5xl py-2 text-blue-600 font-medium dark:text-blue-400 md:text-6xl">Oops!</h2>
-                        <h3 className="text-2xl py-2 dark:text-white md:text-3xl mb-4">404 - Page Not Found</h3>
+                        <h2 className="font-gloriaHallelujah text-5xl py-2 text-blue-600 font-semibold tracking-wide dark:text-blue-400 md:text-6xl">Oops!</h2>
+                        <h3 className="font-gloriaHallelujah text-2xl py-2 dark:text-white md:text-3xl mb-4">404 - Page Not Found</h3>
                         <p className="text-md md:text-xl font-normal py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-lg mx-auto">
                             Perhaps the page you are looking for has risen into the unknown...?
                         </p>
