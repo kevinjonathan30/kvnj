c15847c484cc4d2219a438782c820aa7cced986d
Kevin Jonathan <kevinjonathan3010@gmail.com>
2023-12-06 17:51:26 +0700

    Shorten footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,8 @@
 export default function Footer() {
-    const date = new Date();
-    const year = date.getFullYear();
-
     return (
         <footer className="text-center p-5">
             <p className="text-md md:text-lg text-lg py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-lg mx-auto">
-                Kevin Jonathan © 2018-{year}. All rights reserved.
+                © Kevin Jonathan. All Rights Reserved.
             </p>
         </footer>
     );
